ffc89d5e0b12334450c6f08c5adbae6ac4ad264e
Adrian <adfonat88@yahoo.com>
2021-05-01 21:02:30 +0200

    create readme file

--- src/pages/BackdropFilter.js
+++ src/pages/BackdropFilter.js
@@ -5,18 +5,23 @@ import { IoColorPalette } from "react-icons/io5";
 
 function BackdropFilter() {
     return (
-        <div className={style.background}>
-            <Draggable handle="strong">
-                <div className={style.glass}>
-                    <strong><div className={style.drag}></div></strong>
-                    <div className={`${style.glassClone} ${style.glassContent}`}>
-                        <h1>Welcome to the Mossley gallery</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam voluptatem fugit rerum recusandae autem. Perspiciatis officia delectus amet eius a, beatae aliquid saepe perferendis doloribus aperiam modi minima, magnam fuga!</p>
-                        <p><IoColorPalette className={style.icon} /></p>
+        <>
+            <div className={style.background}>
+                <Draggable handle="strong">
+                    <div className={style.glass}>
+                        <strong><div className={style.drag}></div></strong>
+                        <div className={`${style.glassClone} ${style.glassContent}`}>
+                            <h1>Welcome to the Mossley gallery</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam voluptatem fugit rerum recusandae autem. Perspiciatis officia delectus amet eius a, beatae aliquid saepe perferendis doloribus aperiam modi minima, magnam fuga!</p>
+                            <p><IoColorPalette className={style.icon} /></p>
+                        </div>
                     </div>
-                </div>
-            </Draggable>
-        </div>
+                </Draggable>
+            </div>
+            <div className={style.source}>
+                Photo by <a href="https://unsplash.com/@_calvincraig?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Calvin Craig</a> on <a href="https://unsplash.com/s/photos/michelangelo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            </div>
+        </>
     )
 }
 
